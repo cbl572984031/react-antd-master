f15fb769e3947aec3a15fa6a5bc37a72fae21fc2
cbl572984031 <572984031@qq.com>
2019-11-20 15:35:21 +0800

    路径修改

--- src/page/index.js
+++ src/page/index.js
@@ -4,8 +4,8 @@ import UI from '../page/UI'
 
 const Home = Loading(() => import('../page/home'))
 const Swiper = Loading(() => import('../page/swiper'))
-const Echarts_China = Loading(() => import('../page/Chart/echarts_China'))
-const Draw = Loading(() => import('../page/Chart/Draw_Galaxy'))
+const Echarts_China = Loading(() => import('../page/chart/echarts_China'))
+const Draw = Loading(() => import('../page/chart/Draw_Galaxy'))
 const BasicAnimation = Loading(() => import('../page/Animation/BasicAnimation'))
 
 export default {
